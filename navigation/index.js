ada6174db68642ec539425b7438260fcc65f4c65
jeocamrank <104308268+jeocamrank@users.noreply.github.com>
2023-11-05 13:53:11 +0700

    add folder and add function int to index from nav

---
+++ navigation/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image } from "react-native";
+import { createAppContainer, createStackNavigator} from "react-navigation";
+
+import Welcome from "../screens/Welcome";
+import Login from "../screens/Login";
+import Explore from "../screens/Explore";
+import Browse from "../screens/Browse";
+import Product from "../screens/Product";
+import Settings from "../screens/Settings";
+
+import { theme } from "../constants";
+
+const screen = createStackNavigator({
+    Welcome,
+    Login,
+    Explore,
+    Browse,
+    Product,
+    Settings,
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {},
+        headerBackImage: <Image />,
+        headerBackTitle: null,
+        headerLeftContainerStyle: {},
+        headerRightContainerStyle: {},
+   }
+});
+
+export default createAppContainer(screen)
